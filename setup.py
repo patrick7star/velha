f0ab9974469e31a2ccd60ea48b5bcdb654338f01
Patrick StarCode <90501975+patrick7star@users.noreply.github.com>
2023-11-15 17:57:29 -0300

    parte do código foi organizada.

--- setup.py
+++ setup.py
@@ -44,17 +44,10 @@ from codigo.motor import *
 # bibliote externas:
 from biblioteca_externa.utilitarios.lib import tela
 
-
-# dício contendo todos os DOIS possíveis
-# jogadores.
-jogadores = {
-   True: xis,
-   False: quadrado
-}
-# peça xis, sempre o primeiro.
-primeiro = True
-# instância o tabuleiro para o jogo(inicializa
-# a "interface gráfica").
+from random import choice
+# seleciona peça de forma randômica.
+jogador = choice([Jogadores.XIS, Jogadores.BOLA])
+# instância o tabuleiro para o jogo(inicializa a "interface gráfica").
 tabuleiro = Tabuleiro()
 
 from codigo.ponto import Direcao
@@ -80,124 +73,132 @@ def converte_em_direcoes(code: int) -> Direcao:
 ...
 
 from sys import argv
+# ativa o modo teclado para jogar.
 qtd_args = len(argv)
-uso_do_teclado = (qtd_args > 1 and argv[1] == "--teclado")
+uso_do_teclado = (qtd_args == 2 and "--teclado" == argv[1])
 
-# roda o jogo até não houver mais jogadas.
-while jogadas_restantes() != 0:
-   # desenha todo tabuleiro e seus objetos.
-   tabuleiro.desenha_tudo()
-   # pega 'input' do teclado.
-   tecla = tabuleiro.janela.getch()
-   # pega possível tecla direcional, e converte-a
-   # na sua respectiva Direção.
-   seta = converte_em_direcoes(tecla)
+if (not uso_do_teclado):
+   tabuleiro.desativa_seletor()
 
-   # escape da partida.
-   if tecla == ord('s') or tecla == ord('S'):
-      break
+# roda a parte gráfica do jogo.
+def roda_o_jogo() -> None:
+   global primeiro, jogador
+   # roda o jogo até não houver mais jogadas.
+   while jogadas_restantes() != 0:
+      # desenha todo tabuleiro e seus objetos.
+      tabuleiro.renderiza()
+      # pega 'input' do teclado.
+      tecla = tabuleiro.janela.getch()
 
-   # uso do teclado para jogar
-   colocar_peca_pressionado = (
-      tecla == ord('c') or 
-      tecla == ord('C') or
-      tecla == KEY_ENTER
-   )
-   if uso_do_teclado:
-      if seta is not None:
-         tabuleiro.seletor.move(seta)
+      # escape da partida.
+      if tecla == ord('s') or tecla == ord('S'):
+         break
+
+      # uso do teclado para jogar
+      if uso_do_teclado:
+         # pega possível tecla direcional, e converte-a na sua 
+         # respectiva Direção.
+         seta = converte_em_direcoes(tecla)
          local = tabuleiro.seletor.atual
-         continue
-      elif colocar_peca_pressionado:
-         tabuleiro.coloca_peca(
-            jogadores[primeiro],
-            tabuleiro.seletor.atual
-         )
-      ...
-   # uso do mouse.
-   else:
-      # coordenadas do mouse relativo a janela.
-      coord = tabuleiro.posicoes()
-      try:
-         local = tabuleiro.posicao_clicada(coord)
-         tabuleiro.coloca_peca(jogadores[primeiro], local)
-      except ForaTabuleiroError:
-         tabuleiro.informa_algo("fora do tabuleiro!")
-         # dá chance a outro clique.
-         continue
-      except LocalJaPreenchidoError:
-         tabuleiro.informa_algo("posição já marcada")
-         continue
-      ...
-   ...
-
-   try:
-      # se estiver no primeiro, então adiciona
-      # a peça(1), caso contŕario, peça(2).
-      if primeiro:
-         adiciona_peca(pecas_i, numeracao_em_coord(local))
+         if seta is not None:
+            tabuleiro.seletor.move(seta)
+            continue
+         elif tecla == KEY_ENTER:
+            try:
+               tabuleiro.coloca_peca(jogador, local)
+            except LocalJaPreenchidoError:
+               tabuleiro.informa_algo("posição já preenchida!")
+               continue
+            ...
+         ...
+      # uso do mouse.
       else:
-         adiciona_peca(pecas_ii, numeracao_em_coord(local))
-   except LocalJaPreenchidoError:
-      # voltar para o começo do laço, e
-      # tentar novamente.
-      continue
+         # coordenadas do mouse relativo a janela.
+         coord = tabuleiro.posicoes()
+         try:
+            local = tabuleiro.posicao_clicada(coord)
+            tabuleiro.coloca_peca(jogador, local)
+         except ForaTabuleiroError:
+            tabuleiro.informa_algo("fora do tabuleiro!")
+            # dá chance a outro clique.
+            continue
+         except LocalJaPreenchidoError:
+            tabuleiro.informa_algo("posição já marcada")
+            continue
+         ...
+      ...
+
+      try:
+         adiciona_peca(jogador, numeracao_em_coord(local))
+      except LocalJaPreenchidoError:
+         # voltar para o começo do laço, e tentar novamente.
+         continue
+      finally:
+         alguma_peca_venceu = (
+            peca_vitoriosa(Jogadores.XIS) or
+            peca_vitoriosa(Jogadores.BOLA)
+         )
+         if alguma_peca_venceu:
+            tabuleiro.marca_vitoria(fileira_ganha())
+            break
+
+         # obtem evento do mouse/teclado.
+         # primeiro = (not primeiro)  # alterna de player.
+         jogador = jogador.alternar()
+      ...
    ...
 
-   if peca_vitoriosa(pecas_i) or peca_vitoriosa(pecas_ii):
-      tabuleiro.marca_vitoria(fileira_ganha())
-      break
-   ...
-
-   # obtem evento do mouse/teclado.
-   primeiro = (not primeiro)  # alterna de player.
+   # encerra a "interface do curses".
+   tabuleiro.desmancha_tabuleiro()
 ...
 
-# encerra a "interface do curses".
-tabuleiro.desmancha_tabuleiro()
-
 # mostra resultado do jogo e demais informações.
+def visualizacao_resultado_da_partida() -> None:
+   if peca_vitoriosa(Jogadores.XIS):
+      o_vencedor = r'"xis" GANHOU.'
+   elif peca_vitoriosa(Jogadores.BOLA):
+      o_vencedor = r'"bola" GANHOU.'
+   else:
+      o_vencedor = "jogo EMPATADO."
+   # cria tela de desenho.
+   t = tela.Tela(12, 300, grade=False)
+   # cabeçalho sobre os jogadores.
+   t.escreve(0, 0, "1º jogador: 'X'\t2º jogador: 'O'")
 
-if peca_vitoriosa(pecas_i):
-   o_vencedor = "\"xis\" GANHOU."
-elif peca_vitoriosa(pecas_ii):
-   o_vencedor = "\"quadrado\" GANHOU."
-else:
-   o_vencedor = "jogo EMPATADO."
+   # desenha o vencedor.
+   (linha, coluna) = (4, 10)
+   t.escreve(linha, coluna, o_vencedor)
+   t.enquadra(
+      linha-2, coluna-3,
+      altura = 4,
+      largura = len(o_vencedor) + 4
+   )
 
-# cria tela de desenho.
-t = tela.Tela(12, 300, grade=False)
-# cabeçalho sobre os jogadores.
-t.escreve(0, 0, "1º jogador: 'X'\t2º jogador: 'O'")
+   # screenshot miniaturizado sobre os jogo em geral.
+   t.escreve(0, 40, "como o jogo terminou:")
+   tabuleiro_screenshot = tabuleiro_str().split('\n')
+   while tabuleiro_screenshot.count('') > 0:
+      tabuleiro_screenshot.remove('')
+   arg1 = tabuleiro_screenshot[0]
+   arg2 = tabuleiro_screenshot[1]
+   arg3 = tabuleiro_screenshot[2]
+   arg4 = tabuleiro_screenshot[3]
+   arg5 = tabuleiro_screenshot[4]
+   arg6 = tabuleiro_screenshot[5]
+   arg7 = tabuleiro_screenshot[6]
+   t.lista_strings(2, 43, arg1, arg2, arg3, arg4, arg5, arg6, arg7)
 
-# desenha o vencedor.
-(linha, coluna) = (4, 10)
-t.escreve(linha, coluna, o_vencedor)
-t.enquadra(
-   linha-2, coluna-3,
-   altura = 4,
-   largura = len(o_vencedor) + 4
-)
+   # risco de divisão do resultado.
+   t.risca(1, 34, 9, simbolo='|', horizontal=False)
 
-# screenshot miniaturizado sobre os jogo em geral.
-t.escreve(0, 40, "como o jogo terminou:")
-tabuleiro_screenshot = tabuleiro_str().split('\n')
-while tabuleiro_screenshot.count('') > 0:
-   tabuleiro_screenshot.remove('')
-arg1 = tabuleiro_screenshot[0]
-arg2 = tabuleiro_screenshot[1]
-arg3 = tabuleiro_screenshot[2]
-arg4 = tabuleiro_screenshot[3]
-arg5 = tabuleiro_screenshot[4]
-arg6 = tabuleiro_screenshot[5]
-arg7 = tabuleiro_screenshot[6]
-t.lista_strings(2, 43, arg1, arg2, arg3, arg4, arg5, arg6, arg7)
+   # visualizando tela o resultado geral.
+   print(t)
 
-# risco de divisão do resultado.
-t.risca(1, 34, 9, simbolo='|', horizontal=False)
+   # registrando no banco de screenshots do tabuleiro.
+   resultado_bd = open("dados/resultados.txt", mode="at")
+   print(t, file=resultado_bd)
+...
 
-# visualizando tela o resultado geral.
-print(t)
-# registrando no banco de screenshots do tabuleiro.
-resultado_bd = open("dados/resultados.txt", mode="at")
-print(t, file=resultado_bd)
+# inicializa execução ...
+roda_o_jogo()
+visualizacao_resultado_da_partida()
